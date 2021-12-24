f86887122fc2bfcaffeaa674ceab28d58855bc7b
Roger Freitas <rogereduardofreitas@gmail.com>
2021-12-24 20:24:35 -0300

    Adicionando a anotação DataJpaTest

--- src/test/java/br/com/rogerdesenvolvedorweb/appproduto/AppprodutoApplicationTests.java
+++ src/test/java/br/com/rogerdesenvolvedorweb/appproduto/AppprodutoApplicationTests.java
@@ -1,13 +1,15 @@
 package br.com.rogerdesenvolvedorweb.appproduto;
 
 import org.junit.jupiter.api.Test;
-
+//import org.junit.runner. *;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.test.context.junit4.SpringRunner;
 import br.com.appproduto.AppprodutoApplication;
 
 
 
+@DataJpaTest
 @SpringBootTest(classes = AppprodutoApplication.class)
 class AppprodutoApplicationTests {
 
