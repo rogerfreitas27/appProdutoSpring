8ec6c1fd89dfa926dc4d3d87f1a21ed9d37eb8fd
Roger Freitas <rogereduardofreitas@gmail.com>
2021-12-24 20:53:28 -0300

    ocultando @DataJpaTest na classe de teste

--- src/test/java/br/com/rogerdesenvolvedorweb/appproduto/AppprodutoApplicationTests.java
+++ src/test/java/br/com/rogerdesenvolvedorweb/appproduto/AppprodutoApplicationTests.java
@@ -1,17 +1,17 @@
 package br.com.rogerdesenvolvedorweb.appproduto;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner. *;
+//import org.junit.runner. *;
 //import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import br.com.appproduto.AppprodutoApplication;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import br.com.appproduto.AppprodutoApplication;
 
 
 
 //@DataJpaTest
-@SpringBootTest(classes = AppprodutoApplication.class)
-@RunWith(SpringRunner.class)
+@SpringBootTest()
+//@RunWith(SpringRunner.class)
 class AppprodutoApplicationTests {
 
 	@Test
