7a128464c221db4f97ada24d4c36c15878b8fa3c
Roger Freitas <rogereduardofreitas@gmail.com>
2021-12-24 19:30:32 -0300

    Atualizando teste

--- src/test/java/br/com/rogerdesenvolvedorweb/appproduto/AppprodutoApplicationTests.java
+++ src/test/java/br/com/rogerdesenvolvedorweb/appproduto/AppprodutoApplicationTests.java
@@ -1,9 +1,14 @@
 package br.com.rogerdesenvolvedorweb.appproduto;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+import br.com.appproduto.AppprodutoApplication;
+
+
+
+@SpringBootTest(classes = AppprodutoApplication.class)
 class AppprodutoApplicationTests {
 
 	@Test
