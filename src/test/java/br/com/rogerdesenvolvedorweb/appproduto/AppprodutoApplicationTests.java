529d6a7aad9b6f5c3f83d29556c5c241aa7b7adf
Roger Freitas <rogereduardofreitas@gmail.com>
2021-12-24 21:25:43 -0300

    Atualizando Dockerfile

--- src/test/java/br/com/rogerdesenvolvedorweb/appproduto/AppprodutoApplicationTests.java
+++ src/test/java/br/com/rogerdesenvolvedorweb/appproduto/AppprodutoApplicationTests.java
@@ -3,14 +3,14 @@ package br.com.rogerdesenvolvedorweb.appproduto;
 import org.junit.jupiter.api.Test;
 //import org.junit.runner. *;
 //import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.test.context.junit4.SpringRunner;
 //import br.com.appproduto.AppprodutoApplication;
 
 
 
 //@DataJpaTest
-@SpringBootTest()
+//@SpringBootTest(class.AppProdutoApplication)
 //@RunWith(SpringRunner.class)
 class AppprodutoApplicationTests {
 
